ea162d32aac5539c14b94a9ae433d3871cfff04d
CESAR <eliascastillo2509@gmail.com>
2024-11-20 16:42:12 -0500

    Subida del proyecto API Rest

---
+++ Protecto#2_API-REST_DSIV/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Protecto_2_API_REST_DSIV
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Protecto#2_API-REST_DSIV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Protecto_2_API_REST_DSIV.MvcApplication" Language="C#" %>
