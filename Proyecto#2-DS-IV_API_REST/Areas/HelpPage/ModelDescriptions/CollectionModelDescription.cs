ab226c8c5b3664352cf1198044d66a698244646c
CESAR <eliascastillo2509@gmail.com>
2024-11-20 17:13:42 -0500

    Se realizo un ajuste en el proyecto

---
+++ Proyecto#2-DS-IV_API_REST/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Proyecto_2_DS_IV_API_REST.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Proyecto#2-DS-IV_API_REST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_2_DS_IV_API_REST.WebApiApplication" Language="C#" %>
